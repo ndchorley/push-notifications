614e4e27d85c26904e6eab7c4a21cb2018e7bc08
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-03-24 09:25:55 +0000

    Introduce a route for showing a page

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(platform("org.http4k:http4k-bom:6.37.0.0"))
 
     implementation("org.http4k:http4k-core")
-    implementation("org.http4k:http4k-server-jetty")    
+    implementation("org.http4k:http4k-server-jetty")
 }
 
 kotlin {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-import org.http4k.core.Method.POST
+import org.http4k.core.Method.*
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
@@ -6,9 +6,11 @@ import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.nio.file.Files
+import java.nio.file.Paths
 
 fun main() {
-    val routes = routes(personIsHere)
+    val routes = routes(personIsHere, showPage)
 
     routes.asServer(Jetty(port = 9000)).start()
 }
@@ -26,4 +28,15 @@ fun notifyClientsAbout(name: String) {
     val clients: List<Client> = listOf(ConsoleClient)
 
     clients.forEach { client -> client.notify(name) }
+}
+
+val showPage =
+    "/" bind GET to { _ ->
+        Response(OK).body(page())
+    }
+
+fun page(): String {
+    return Files.readString(
+        Paths.get("src/main/resources/page.html")
+    )
 }
